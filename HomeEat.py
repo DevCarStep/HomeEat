191122c66eefc0617fb8249c5772a96d196d1a59
CarStep <carstep9823@gmail.com>
2025-05-07 14:10:03 +0300

    Trying make some more
    - added user button functions

--- HomeEat.py
+++ HomeEat.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import showinfo
  
 root = Tk()     # создаем корневой объект - окно
 root.title("HomeEat")     # устанавливаем заголовок окна
@@ -14,13 +15,31 @@ upper_frame_style.configure("TFrame", background="white")
 canvas = Canvas(upper_frame, background="white", width=72, height=72, highlightthickness=0)
 canvas.pack(anchor="nw", side=LEFT)
 
+def UserCircleClick(event):
+    showinfo(title="Информация", message="Кликер работает!")
+def BrightlessUp(event):
+    more_bright = int(user_collor, base=16) + int(222222, base=16)
+    canvas.itemconfigure("weakbutton", fill="#" + str(more_bright))
+    print(more_bright)
+def BrightlessDown(event): canvas.itemconfigure("weakbutton", fill=user_collor)
+username = "Абчихба"
+user_collor = "777777"
+user_canvas = Canvas(upper_frame, background="white", highlightthickness=0, height=60, width=60)
+user_canvas.pack(anchor=NE, side=RIGHT)
+idOval = user_canvas.create_oval(2, 2, 58, 58, fill="#777777", outline="#777777", tags=["clickable", "weakbutton"])
+user_first_letter = username.upper
+user_canvas.create_text(30.5, 30, text=username[0], fill="white", font=("Arial", 20))
+user_canvas.bind("<ButtonPress-1>", UserCircleClick)
+user_canvas.bind("<Enter>", BrightlessUp)
+user_canvas.bind("<Leave>", BrightlessDown)
+
 adress = "Луначарского, 66"
 label = ttk.Label(upper_frame, text=adress, background="white", font= ("Arial", 24))
 label.pack()
 
-user_btn = ttk.Button(upper_frame, text="A")
-user_btn.place(height=60, width=60, anchor=NE)
-user_btn.pack(anchor="ne", side=RIGHT)
+# user_btn = ttk.Button(upper_frame, text="A")
+# user_btn.place(height=60, width=60, anchor=NE)
+# user_btn.pack(anchor="ne", side=RIGHT)
 
 logo = PhotoImage(file=r"C:\Users\224\Desktop\HomeEat\IMG_0843 2.png")
 logo_label = ttk.Label(upper_frame, image=logo)
